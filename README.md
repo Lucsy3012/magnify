6495467401438ea70ae19043bc2b62b9650fb104
Lucas Dietrich <lucas.dietrich@gmx.de>
2019-07-31 22:58:38 +0200

    complete v1

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # magnify
 A small function that allows the user to magnify any image within a specfic holder to fullsize (like a lightbox). The image expands across the whole visible viewport.
+
+## How to use
+
+    <!-- initiate an image to magnify -->
+    <div class="magnify">
+        <img src="#" />
+    </div>
+    
+    <!-- you can change the space around in pixels (initial: 40) -->
+    <div class="magnify">
+        <img src="#" data-space-around="60" />
+    </div>
---
+++ src/js/magnify.js
@@ -0,0 +1,119 @@
+$(document).ready(function () {
+
+    var $body = $('body'),
+        activeClass = 'magnified';
+
+    adaptHeight();
+
+    $(window).on('scroll', function() {
+        adaptHeight();
+    });
+
+    $(window).on('resize', function() {
+        adaptHeight();
+
+        $('.magnify.magnified').each(function() {
+            changePosition($(this), $(this).find('img'), 40);
+        });
+    });
+
+    // Add backdrop to every magnifiable image
+    $body.find('.magnify').append('<div class="backdrop"></div>');
+
+    // Reset with click on backdrop
+    $body.on('click', '.magnify .backdrop', function() {
+        reset();
+    });
+
+    // Magnify
+    $body.on('click', '.magnify img', function(e) {
+        magnify($(this));
+    });
+
+    function getRatio($el) {
+        return 100 / $el.width() * $el.height();
+    }
+
+    function adaptHeight() {
+        $('.magnify').each(function() {
+            var $holder = $(this),
+                $img = $holder.find('img');
+
+            // get necessary height
+            var ratio = getRatio($img),
+                ratioHeight = $holder.width() / 100 * ratio;
+
+            $holder.css('height', ratioHeight);
+        });
+    }
+
+    function magnify($el) {
+        var $parent = $el.parents('.magnify'),
+            spaceAround = parseFloat($el.attr('data-space-around')),
+            _magnified = $parent.hasClass(activeClass);
+
+        reset();
+
+        // is currently not magnified
+        if (!_magnified) {
+            $parent.addClass(activeClass);
+            changePosition($parent, $el, spaceAround);
+        } else {
+            reset();
+        }
+    }
+
+    function changePosition($holder, $img, spaceAround) {
+        if ($img.attr('data-space-around') !== undefined) {
+            spaceAround = parseFloat($img.attr('data-space-around'));
+        } else {
+            spaceAround = 40;
+        }
+
+        console.log(spaceAround);
+
+        var top, left;
+
+        // get window dimensions
+        var $window = $(window),
+            windowWidth = $window.width() - (spaceAround * 2),
+            windowHeight = $window.height() - (spaceAround * 2);
+
+        // get necessary height
+        var ratio = getRatio($img),
+            ratioWidth = windowHeight / ratio * 100,
+            ratioHeight = windowWidth / 100 * ratio;
+
+        // if portrait mode image
+        if (ratio > 100 || windowHeight + (spaceAround * 2) < ratioHeight + (spaceAround * 2)) {
+            top = 0 - $holder.offset().top + $window.scrollTop() + spaceAround;
+            left = ($window.width() / 2) - (ratioWidth / 2) - $holder.offset().left;
+
+            $img.css({
+                'top': top,
+                'left': left,
+                'width': ratioWidth
+            });
+
+        } else {
+            top = $window.scrollTop() + ($window.height() / 2) - (ratioHeight / 2) - $holder.offset().top;
+            left = 0 - $holder.offset().left + spaceAround;
+
+            $img.css({
+                'top': top,
+                'left': left,
+                'width': windowWidth
+            });
+        }
+    }
+
+    function reset() {
+        $('.magnify img').css({
+            'top': 0,
+            'left': 0,
+            'width': '100%'
+        });
+        $('.magnify').removeClass(activeClass);
+    }
+
+});
